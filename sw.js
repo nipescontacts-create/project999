2caf3df3bdcb905087338147bef69c096fe50c58
nipescontacts-create <nipes.contacts@gmail.com>
2026-04-19 11:49:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'p99-v3';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      var network = fetch(e.request).then(function(res) {
+        if (res.ok) caches.open(CACHE).then(function(c) { c.put(e.request, res.clone()); });
+        return res;
+      }).catch(function() { return cached; });
+      return cached || network;
+    })
+  );
+});
